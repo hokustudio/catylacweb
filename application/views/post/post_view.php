ac9ce17423748039524e64489b1cb6d8898d127b
fuadmakarim <fuadmakarim@outlook.com>
2015-05-17 13:58:37 +0700

    new post_view

---
+++ application/views/post/post_view.php
@@ -0,0 +1,205 @@
+<body>
+    <div class="container" style="margin-top:120px;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                    <div class="col-md-12">
+                        <div class="cumbread">
+                            <a href="#">Beranda</a><span> > </span><a href="#">Inspirasi</a><span> > </span><a>Tips & Trik</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+               
+                    <div class="col-md-12">
+                        <h1 class="title">Tips Mengecat Rumah</h1>
+                        <div class="meta">
+                            <span class="date">5 Februari 2015</span>
+                            <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                            <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                        </div>
+                        <img class="img-responsive" src="images/ar1.jpg" alt="catylac"/>
+                        <p class="content" style="margin-top:20px;">    Selain sebagai tempat bunga, vas bunga, juga bisa dijadikan hiasan untuk mempercantik rumah. Bentuknya yang unik dan kreatif memberi kesan tersendiri. Berpadu dengan dinding bercat warna cerah keindahan yang artistic langsung terasa. Dan bila vas berisi bunga, nuansa fresh dan segar akan selalu hadir di rumah Anda. Jadi terpikir untuk meletakkan vas bunga di salah satu sudut rumah? Lakukan saja</p>
+                    </div>
+                    <div class="col-md-12" style="margin-top:20px;">
+                        <a class="btn btn-facebook" href="#">
+                            <i class="fa fa-facebook-square fa-lg"></i> Facebook</a>
+                        <a class="btn btn-twitter" href="#">
+                            <i class="fa fa-twitter fa-lg"></i> Twitter</a>
+                        <a class="btn btn-google" href="#">
+                            <i class="fa fa-google fa-lg"></i> Google+</a>
+                        <a class="btn btn-pinterest" href="#">
+                            <i class="fa fa-pinterest fa-lg"></i> Pinterest</a>
+                    </div>
+                    <div class="col-md-12" style="margin-top:20px;">
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        
+                        </div>   
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="body-col">
+                            <div class="col-md-6" style="margin-top:20px; margin-left:-20px;">
+                                <div class="col-md-4 col-lg-4">
+                                    <div class="img-sm">
+                                        <img src="images/tekno.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-8">
+                                    <h1 class="title2"><a href="tekno-cat.php">Teknologi Cat Exterior Yang Perlu Anda Ketahui</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6" style="margin-top:20px; margin-left:-20px;">
+                                <div class="col-md-4 col-lg-4">
+                                    <div class="img-sm">
+                                        <img src="images/wall.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-8">
+                                    <h1 class="title2"><a href="wall-sticker.php">Tips menempel wall sticker</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="body-col">
+                            <div class="col-md-6" style="margin-top:20px; margin-left:-20px;">
+                                <div class="col-md-4 col-lg-4">
+                                    <div class="img-sm">
+                                        <img src="images/ruangkecil.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-8">
+                                    <h1 class="title2"><a href="tips-membuat-ruangan-kecil-berkesan-luas.php">Tips membuat ruangan kecil berkesan luas</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6" style="margin-top:20px; margin-left:-20px;">
+                                <div class="col-md-4 col-lg-4">
+                                    <div class="img-sm">
+                                        <img src="images/tips.jpg"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-8">
+                                    <h1 class="title2"><a href="tips-mengecat.php">Tips Mengecat Rumah Teknologi Cat yang Perlu Diketahui</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12" style="margin-top:30px;">
+                        <div class="well">
+                            <h4>Leave a Comment:</h4>
+                            <form role="form">
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="name" id="name" placeholder="Name"/> 
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="email" id="email" placeholder="Email"/> 
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="website" id="website" placeholder="Website"/> 
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="3">Comment</textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div class="col-md-12 hidden-sm hidden-xs" style="padding-left:0px; padding-right:0px;">
+                        <form role="form">
+                            <div class="input-group">
+                                <input class="form-control" type="text" name="search" id="search" placeholder="Search"/> 
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-12" style="margin-top:20px; padding-left:0px; padding-right:0px;">
+                        <div class="col-md-8 panelEvent" style="background-color:#1c74bb;">
+                            <h1>Artikel Populer</h1>
+                        </div>
+                        <div class="col-md-12 lineB">
+                        </div>
+                    </div>
+                    <div class="col-md-12" style="margin-top:20px; background-color:#f1c143; padding-top:20px; padding-bottom:20px;">
+                            <img class="img-responsive" src="images/tinting.jpg" alt="Padu padan warna kesukaan"/>
+                            <h1 class="title">Mau Eksplorasi Warna!? Tinting-in aja….</h1>
+                            <div class="meta">
+                                <span class="date">5 Februari 2015</span>
+                                <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                            </div>
+                            <p class="content">Agar rumah kesayangan terlihat indah sering kali kita ingin mengecatnya dengan warna istimewa hasil dari eksplorasi ide sendiri.
+                            <a href="tinting.php">read more</a></p>
+                            <div class="col-md-12" style="margin-top:20px; padding-left:0px; padding-right:0px;">
+                                <div class="col-md-4" style="padding:0px;">
+                                    <img class="img-responsive" src="images/exterioe.jpg" alt="cat"/>
+                                </div>
+                                <div class="col-md-8">
+                                    <h1 class="title2"><a href="tips-mengecat-exterior.php">Waktu yang Tepat Mengecat Exterior</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                             <div class="col-md-12" style="margin-top:20px; padding-left:0px; padding-right:0px;">
+                                <div class="col-md-4" style="padding:0px;">
+                                    <img class="img-responsive" src="images/ruang.jpg" alt="cat"/>
+                                </div>
+                                <div class="col-md-8">
+                                    <h1 class="title2"><a href="Ruang-kerja-minimalis.php">Menciptakan Ruang Baca dan Kerja di Rumah Minimalis</a></h1>
+                                    <div class="meta">
+                                        <span class="date">5 Februari 2015</span>
+                                        <span class="comment"><span class="glyphicon glyphicon-comment"></span> 3</span>
+                                        <span class="views"><span class="glyphicon glyphicon-eye-open"></span> 10</span>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="col-md-12  text-center" style="padding-left:0px; padding-right:0px;">
+                        <div class="newsletter">
+                        <h1>Newsletter</h1>
+                        <p>Dapatkan update terbaru dari catylac</p>
+                        <form role="form" method="post" style="padding:0 10%;">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="newsletter" placeholder="emailanda@email.com"/><br/>
+                                <button type="submit" class="btn btn-default" name="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
