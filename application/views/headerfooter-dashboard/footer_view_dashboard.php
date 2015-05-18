fa93a4f5cc414becaf8812e6e681b230fef2d4ef
fuadmakarim <fuadmakarim@outlook.com>
2015-05-19 00:22:41 +0700

    add header footer for dashboard

---
+++ application/views/headerfooter-dashboard/footer_view_dashboard.php
@@ -0,0 +1,20 @@
+    <!-- jQuery -->
+    <script src="dashboard/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="dashboard/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="dashboard/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="dashboard/bower_components/raphael/raphael-min.js"></script>
+    <script src="dashboard/bower_components/morrisjs/morris.min.js"></script>
+    <script src="dashboard/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="dashboard/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
