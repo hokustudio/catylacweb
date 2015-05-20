5a5931a2b93b6f765e1228dfb959226837e14320
dap1006 <dwiagungprastya@hotmail.com>
2015-05-20 19:36:35 +0700

    leave comment bisa diklik

---
+++ application/views/coba/leave_comment.php
@@ -0,0 +1,20 @@
+<div class="well">
+                            <h4>Leave a Comment:</h4>
+                            <form role="form" action="<?php echo base_url()?>" method="post">
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="name" id="name" placeholder="Name"/> 
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="email" id="email" placeholder="Email"/> 
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" type="text" name="website" id="website" placeholder="Website"/> 
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary" value="submit">Submit</button>
+                                </div>
+                            </form>
+                        </div>
