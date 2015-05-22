2a98a5d4e8c80ba535efd8a3ac38237395fc6321
aimzeter <mzrausyanfikri@gmail.com>
2015-05-23 01:15:00 +0700

    Tambah tabel admin

--- application/views/admin/admin_all_post_view.php
+++ application/views/admin/admin_all_post_view.php
@@ -12,18 +12,20 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
-                                	<ul>
 									<?php 
 										for($i=0;$i<$total;$i++){ ?>
-											<li>
-												<a href= "<?php echo base_url().'catylacadmin2015/editpost/'; echo $id[$i] ?>"><?php echo $title[$i] ?></a><?php echo $author[$i]." ".$category[$i]." ".$date[$i]." ".$status[$i] ?>
-<?php if($flag[$i] == 2){ ?><a href="<?php echo base_url().'catylacadmin2015/publish/'; echo $id[$i] ?>" >Publish Post</a> <?php } ?>
-<?php if($flag[$i] == 1){ ?><a href="<?php echo base_url().'catylacadmin2015/draft/'; echo $id[$i] ?>" >Draft Post</a> <?php } ?>
-												<a href="<?php echo base_url().'catylacadmin2015/deletepost/'; echo $id[$i] ?>" >Delete</a>
-											</li>
+											<tr>
+												<td><a href= "<?php echo base_url().'catylacadmin2015/editpost/'; echo $id[$i] ?>"><?php echo $title[$i] ?></a></td>
+												<td><?php echo $author[$i] ?></td>
+												<td> <?php echo $category[$i] ?></td>
+												<td><?php $date[$i]?><?php $status[$i] ?></td>
+												<td><?php if($flag[$i] == 2){ ?><a href="<?php echo base_url().'catylacadmin2015/publish/'; echo $id[$i] ?>" >Publish Post</a> <?php } ?>
+												<?php if($flag[$i] == 1){ ?><a href="<?php echo base_url().'catylacadmin2015/draft/'; echo $id[$i] ?>" >Draft Post</a> <?php } ?></td>
+												<td><a href="<?php echo base_url().'catylacadmin2015/deletepost/'; echo $id[$i] ?>" >Delete</a></td>
+											</tr>
 									<?php	}
 									?>
-									</ul>
+									</table>
 								</div>
 							</div>
 						</div>
