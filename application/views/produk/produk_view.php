fec2ecc03ca7cba31f5349097bb6ee85f264f8e4
fuadmakarim <fuadmakarim@outlook.com>
2015-05-23 05:10:17 +0700

    png

--- application/views/produk/produk_view.php
+++ application/views/produk/produk_view.php
@@ -1,6 +1,7 @@
 <div class="container">
     
     <!-- Nav tabs -->
+  <div class="col-md-10 col-md-offset-1">
     <ul class="nav nav-tabs" role="tablist">
       <li class="active">
           <a href="#home" role="tab" data-toggle="tab">
@@ -21,17 +22,150 @@
     <!-- Tab panes -->
     <div class="tab-content">
       <div class="tab-pane fade active in" id="home">
-          <h2>Interior</h2>
-          <img src="http://lorempixel.com/400/400/cats/1" alt="Cats"/>
+          <br/>
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="<?php echo base_url()."assets/images/tembok.png"?>"/>
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="title">Dulux Catylac Cat Tembok Interior</h1>
+                    <p>Dulux Catylac Interior adalah cat dengan teknologi Chroma-Bright, menghadirkan warna keindahan cerah menakjubkan pada dinding interior.
+                      <br/></p>
+                    <table>
+                        <tr>
+                            <th>Daya Sebar</th>
+                            <th>: 8 - 10 M/Kg</th>
+                        </tr>
+                        <tr>
+                            <th>Waktu Pengeringan</th>
+                            <th>: 1 - 2 Jam</th>
+                        </tr>
+                        <tr>
+                            <th>Kemasan</th>
+                            <th>: 5 Kg & 25 Kg</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+          <br/>
+
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="<?php echo base_url()."assets/images/dasar.png"?>"/>
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="title">Dulux Catylac Cat Dasar Interior</h1>
+                    <p>Mampu meningkatkan daya rekat pada dinding agar tidak mudah mengembung dan mengelupas.<br/></p>
+                    <table>
+                        <tr>
+                            <th>Daya Sebar</th>
+                            <th>: 7 - 10 M/Kg</th>
+                        </tr>
+                        <tr>
+                            <th>Waktu Pengeringan</th>
+                            <th>: 1 - 2 Jam</th>
+                        </tr>
+                        <tr>
+                            <th>Kemasan</th>
+                            <th>: 4 Kg & 21 Kg</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+          <br/>
+
+           <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="<?php echo base_url()."assets/images/plamur.png"?>"/>
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="title">Dulux Catylac Plamur Interior</h1>
+                    <p>Menutup retak rambut maupun retak kecil pada dinding interior agar hasil akhir sempurna.<br/></p>
+                    <table>
+                        <tr>
+                            <th>Daya Sebar </th>
+                            <th>: 8 - 10 M/Kg</th>
+                        </tr>
+                        <tr>
+                            <th>Waktu Pengeringan </th>
+                            <th>: 1 - 2 Jam</th>
+                        </tr>
+                        <tr>
+                            <th>Kemasan </th>
+                            <th>: 5 Kg & 25 Kg</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+         
       </div>
+      
       <div class="tab-pane fade" id="profile">
-          <h2>Exterior</h2>
-          <img src="http://lorempixel.com/400/400/cats/2" alt="Cats"/>
+          <br/>
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="<?php echo base_url()."assets/images/tembokex.png"?>"/>
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="title">Dulux Catylac Cat Dinding Exterior</h1>
+                    <p>Cat tembok eksterior dengan teknologi Chroma Bright-UV Fight dan Durabonn Latex. Menjaga warna dinding eksterior cerah lebih lama, tahan terhadap cuaca, dan tidak mudah mengelupas.
+                      <br/></p>
+                    
+                    <table>
+                        <tr>
+                            <th>Daya Sebar </th>
+                            <th>: 8 - 10 M/Kg</th>
+                        </tr>
+                        <tr>
+                            <th>Waktu Pengeringan </th>
+                            <th>: 1 - 2 Jam</th>
+                        </tr>
+                        <tr>
+                            <th>Kemasan </th>
+                            <th>: 5 Kg & 25 Kg</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+          <br/>
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="<?php echo base_url()."assets/images/dasarex.png"?>"/>
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="title">Dulux Catylac Cat Dasar Exterior</h1>
+                    <p>Mampu mengurangi serangan garam alkali dan meningkatkan daya rekat pada dinding agar tidak mudah mengembung dan mengelupas.<br/></p>
+                    <table>
+                        <tr>
+                            <th>Daya Sebar </th>
+                            <th>: 7 - 10 M/Kg</th>
+                        </tr>
+                        <tr>
+                            <th>Waktu Pengeringan </th>
+                            <th>: 1 - 2 Jam</th>
+                        </tr>
+                        <tr>
+                            <th>Kemasan </th>
+                            <th>: 4 Kg & 21 Kg</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
       </div>
       <div class="tab-pane fade" id="messages">
           <h2>Pilihan</h2>
           <img src="http://lorempixel.com/400/400/cats/3" alt="Cats"/>
       </div>
     </div>
-    
+  </div>
 </div>
