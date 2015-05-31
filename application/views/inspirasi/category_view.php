4bf35a98ac0b5fb095e815f760df072e9682de11
dap1006 <dwiagungprastya@hotmail.com>
2015-05-31 17:44:47 +0700

    breadcrumb

--- application/views/inspirasi/category_view.php
+++ application/views/inspirasi/category_view.php
@@ -1,4 +1,14 @@
     <div class="container" style="margin-top:50px;">
+        <div class="col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                <div class="col-md-12">
+                    <div class="cumbread">
+                        <a href="<?php echo base_url().'home' ?>">Beranda</a><span> > </span><a href="#">Inspirasi</a><span> > </span><a href="<?php echo base_url().'inspirasi/tipstrik' ?>">Tips & Trik</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <?php 
         for($i=0;$i<count($on['id']);$i++)
         { ?>
---
+++ application/views/promo/promo_view.php
@@ -0,0 +1,11 @@
+<div class="container" style="margin-top:50px;">
+        <div class="col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                <div class="col-md-12">
+                    <div class="cumbread">
+                        <a href="<?php echo base_url().'home' ?>">Beranda</a><span> > </span><a href="<?php echo base_url().'promo' ?>">Acara & Promosi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
